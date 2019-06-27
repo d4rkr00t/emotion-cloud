0910a081f6d704352af450e442c03941c05493cb
Nathan Flew <nflew@atlassian.com>
2019-06-27 14:00:55 +1000

    Hook up locations

--- src/Mood.svelte
+++ src/Mood.svelte
@@ -1,29 +1,40 @@
 <script>
-  import { slide } from 'svelte/transition';
+  import { slide } from "svelte/transition";
 
-  import Love from './emotions/Love.svelte';
-  import Excited from './emotions/Excited.svelte';
-  import Joy from './emotions/Joy.svelte';
-  import Sad from './emotions/Sad.svelte';
-  import Fear from './emotions/Fear.svelte';
-  import Angry from './emotions/Angry.svelte';
-  import  { addEmotion } from './db.js';
-  
-  const emotionSize = '24px'
+  import Love from "./emotions/Love.svelte";
+  import Excited from "./emotions/Excited.svelte";
+  import Joy from "./emotions/Joy.svelte";
+  import Sad from "./emotions/Sad.svelte";
+  import Fear from "./emotions/Fear.svelte";
+  import Angry from "./emotions/Angry.svelte";
+  import { addEmotion } from "./db.js";
+  import { determineLocation } from "./location.js";
+
+  const emotionSize = "24px";
+
+  let officeLocation;
+  function getOfficeLocation(pos) {
+    const loc = determineLocation(pos.coords);
+    officeLocation = loc;
+  }
 
   let selectedEmotion = { component: Joy };
 
   const emotions = [
-    { id: 3, name: 'Love',   component: Love   },
-    { id: 2, name: 'Excited', component: Excited },
-    { id: 1, name: 'Joy',  component: Joy  },
-    { id: 6, name: 'Sad',  component: Sad  },
-    { id: 5, name: 'Fear',  component: Fear  },
-    { id: 4, name: 'Angry',  component: Angry  },
+    { id: 0, name: "Love", component: Love },
+    { id: 1, name: "Excited", component: Excited },
+    { id: 2, name: "Joy", component: Joy },
+    { id: 3, name: "Sad", component: Sad },
+    { id: 4, name: "Fear", component: Fear },
+    { id: 5, name: "Angry", component: Angry }
   ];
 
   let moodPickerOpen = false;
   function toggleMoodPicker() {
+    if (!officeLocation && moodPickerOpen === false && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getOfficeLocation);
+    }
+
     moodPickerOpen = !moodPickerOpen;
   }
 
@@ -31,9 +42,8 @@
     console.log(`You picked: ${emotionId}`);
     selectedEmotion = emotions.find(emo => emo.id === emotionId);
     moodPickerOpen = false;
-    addEmotion({ emotion: emotionId })
+    addEmotion({ emotion: emotionId, location: officeLocation.id });
   }
-
 </script>
 
 <style>
@@ -50,7 +60,8 @@
     margin: 0;
   }
 
-  .emotion-wrapper button, .mood-toggle {
+  .emotion-wrapper button,
+  .mood-toggle {
     background: none;
     border: none;
     cursor: pointer;
@@ -59,16 +70,28 @@
 
 <div class="mood-wrapper">
   {#if moodPickerOpen}
-    <div class="emotion-wrapper" transition:slide="{{ delay: 250, duration: 300 }}">
+    <div
+      class="emotion-wrapper"
+      transition:slide={{ delay: 250, duration: 300 }}>
       {#each emotions as emotion}
-        <button title={emotion.name} on:click="{(e) => handleMoodSelection(e, emotion.id)}">
-          <svelte:component this={emotion.component} width={emotionSize} height={emotionSize} />
-        </button>  
+        <button
+          title={emotion.name}
+          on:click={e => handleMoodSelection(e, emotion.id)}>
+          <svelte:component
+            this={emotion.component}
+            width={emotionSize}
+            height={emotionSize} />
+        </button>
       {/each}
     </div>
   {/if}
-  
+
   <button class="mood-toggle" on:click={toggleMoodPicker}>
-    <svelte:component this={selectedEmotion.component} width="34px" height="34px" />
+    <svelte:component
+      this={selectedEmotion.component}
+      width="34px"
+      height="34px" />
   </button>
+
+  {#if officeLocation}{officeLocation.name}{/if}
 </div>
