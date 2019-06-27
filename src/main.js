9cb5b6048a01038c1e8c5305913c7aaf53902e2e
Stanislav Sysoev <d4rkr00t@gmail.com>
2019-06-27 22:21:32 +1000

    disable zoom

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
-import App from './App.svelte';
-import { initializeDb } from './db';
+import App from "./App.svelte";
+import { initializeDb } from "./db";
 
 initializeDb();
 
-const app = new App({
-  target: document.body,
-  props: {
-    name: 'world'
-  }
-});
+const app = new App({ target: document.body });
 
 export default app;
