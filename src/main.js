d2ba9afd295957bc2e6af3afb102157487be081c
Rifat Nabi <to.rifat@gmail.com>
2019-06-27 21:28:17 +1000

    Add custom tooltip with %

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import App from './App.svelte';
 import { initializeDb } from './db';
-import { determineLocation } from './location';
-
-console.log(determineLocation({ latitude: -33.86882, longitude: 151.20929 }));
-console.log(determineLocation({ latitude: 52.370216, longitude: 4.895168 }));
-console.log(determineLocation({ latitude: 37.780079, longitude: -122.420174 }));
 
 initializeDb();
 
